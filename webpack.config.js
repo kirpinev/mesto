ae24c58902ee1ee31ea973652886a0bdccac828c
Igor Kirpinev <ikirpinev@gmail.com>
2020-01-26 14:35:37 +0300

    Исправил оптимизацию картинок

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,28 @@ module.exports = {
             options: {
               esModule: false
             }
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65
+              },
+              optipng: {
+                enabled: false
+              },
+              pngquant: {
+                quality: [0.65, 0.9],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              webp: {
+                quality: 75
+              }
+            }
           }
         ]
       },
